e6b93e95a10570c0172485004d5ef5d880750fa3
AbhishekVabilisetty <vabilisettiabhishek@gmail.com>
2024-11-25 22:28:30 +0530

    Your commit message for the new release

--- app/src/main/java/com/example/rewards/MainActivity.java
+++ app/src/main/java/com/example/rewards/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.rewards;
 
-
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -10,10 +10,10 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.ImageButton;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.browser.customtabs.CustomTabsIntent;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -22,11 +22,13 @@ import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
     private boolean isLoadingQueries=false;
+    private boolean isDesktop=false;
     private WebView webView;
     private Button loadQueriesButton;
+    private ImageButton imageButton;
     private Button signOutButton;
     private int count;
-    private ImageView homeButton;
+    private ImageButton homeButton;
     private EditText searchCount;
     private String[] queryList;
     private int queryIndex = 0;
@@ -42,19 +44,18 @@ public class MainActivity extends AppCompatActivity {
         searchCount=findViewById(R.id.editTextText1);
 
 
-
-        webView = findViewById(R.id.webView);
         loadQueriesButton = findViewById(R.id.loadQueriesButton);
         signOutButton = findViewById(R.id.signOutButton);
         signOutButton.setOnClickListener(v -> signOut());
-
+        imageButton=findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(v -> openBingInDesktopMode());
         // Configure WebView settings
+        webView = findViewById(R.id.webView);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient());
-
-        // Load the URL
         webView.loadUrl("https://www.bing.com");
+
 
         // Load queries when button is clicked
         loadQueriesButton.setOnClickListener(new View.OnClickListener() {
@@ -66,8 +67,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void loadQueries() {
-        count=Integer.parseInt(searchCount.getText().toString());
+
         try {
+            count=Integer.parseInt(searchCount.getText().toString());
             if(!isLoadingQueries){
                 isLoadingQueries=true;
             }
@@ -77,7 +79,6 @@ public class MainActivity extends AppCompatActivity {
             String line = reader.readLine();
             if (line != null) {
                 queryList = line.replace(" ", "+").split(",");
-                queryIndex = 0; // Reset query index
                 loadNextQuery();
                 count++;
             } else {
@@ -90,30 +91,50 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void loadNextQuery() {
-        if (  (queryIndex+1)!=count && queryList != null && queryIndex < queryList.length && isLoadingQueries) {
-            String query = queryList[queryIndex];
-            webView.setWebViewClient(new WebViewClient());
-            String url = "https://www.bing.com/search?q=" + query + "&PC=U316&FORM=CHROMN#";
-            webView.loadUrl(url);
-            queryIndex++;
+        if (  queryList != null  && isLoadingQueries) {
 
+            String query = queryList[queryIndex];
+            String url = "https://www.bing.com/search?q=" + query + "&PC=U316&FORM=CHROMN#";
             // Load the next query after a delay
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    loadNextQuery();
-                }
-            }, 7000); // 1-second delay
+            if(isDesktop) {
+                CustomTabsIntent customTabsIntent = new CustomTabsIntent.Builder().build();
+                customTabsIntent.launchUrl(this, Uri.parse(url));
+            }else {
+                webView.loadUrl(url);
+            }
+            queryIndex = (queryIndex + 1) % queryList.length;
+            count--;
+            if(count>0) {
+                // Load the next query after a delay
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        loadNextQuery();
+                    }
+                }, 7000);
+            }else {
+                isLoadingQueries = false;
+                goHome();
+            }
         } else {
             isLoadingQueries=false;
             goHome();
             Toast.makeText(this, "All queries loaded or stopped.", Toast.LENGTH_SHORT).show();
         }
 
-
+    }
+    private void openBingInDesktopMode(){
+        if(!isDesktop){
+            isDesktop=true;
+            Toast.makeText(this, "Desktop Mode Activated Click Start.", Toast.LENGTH_SHORT).show();
+        }else{
+            isDesktop=false;
+            Toast.makeText(this, "Desktop Mode Deactivated.", Toast.LENGTH_SHORT).show();
+        }
     }
     private void goHome(){
         isLoadingQueries=false;
+        webView.clearHistory();
         webView.loadUrl("https://www.bing.com");
     }
     private void signOut() {
